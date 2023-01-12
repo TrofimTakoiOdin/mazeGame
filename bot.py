33b00f417245d75e6467ba6e865093379255dcd5
TrofimTakoiOdin <arseniy.trofimov1993@gmail.com>
2023-01-12 14:17:14 +0300

    added main

--- bot.py
+++ bot.py
@@ -1,98 +1,98 @@
 import telebot
 from telebot import types
-from telebot.types import ReplyKeyboardMarkup
+
 
 from brain import get_map_cell as gm
 
 
+def main():
+    with open("token.txt", "r", encoding="utf-8") as data:
+        token = ""
+        for i in data:
+            token += i
 
-with open("token.txt", "r", encoding="utf-8") as data:
-    token = ""
-    for i in data:
-        token += i
+    bot = telebot.TeleBot(token)
+    cols, rows = 8, 8
 
-bot = telebot.TeleBot(token)
-cols, rows = 8, 8
+    keyboard = telebot.types.InlineKeyboardMarkup()
 
-keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(telebot.types.InlineKeyboardButton('◄', callback_data='left'),
+                 telebot.types.InlineKeyboardButton('▲', callback_data='up'),
+                 telebot.types.InlineKeyboardButton('▼', callback_data='down'),
+                 telebot.types.InlineKeyboardButton('►', callback_data='right'))
 
-keyboard.row(telebot.types.InlineKeyboardButton('◄', callback_data='left'),
-             telebot.types.InlineKeyboardButton('▲', callback_data='up'),
-             telebot.types.InlineKeyboardButton('▼', callback_data='down'),
-             telebot.types.InlineKeyboardButton('►', callback_data='right'))
+    maps = {}
 
-maps = {}
+    def get_map_str(map_cell, player):
+        map_str = ""
+        for y in range(rows * 2 - 1):
+            for x in range(cols * 2 - 1):
+                if map_cell[x + y * (cols * 2 - 1)]:
+                    map_str += "⬛"
+                elif (x, y) == player:
+                    map_str += "🔴"
+                else:
+                    map_str += "⬜"
+            map_str += "\n"
 
-def get_map_str(map_cell, player):
-    map_str = ""
-    for y in range(rows * 2 - 1):
-        for x in range(cols * 2 - 1):
-            if map_cell[x + y * (cols * 2 - 1)]:
-                map_str += "⬛"
-            elif (x, y) == player:
-                map_str += "🔴"
-            else:
-                map_str += "⬜"
-        map_str += "\n"
+        return map_str
 
-    return map_str
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        hello = f'Привет, <b>{message.from_user.first_name}! Сыграем? </b> \nПравила игры: дойдите до правого нижнего угла.'
+        #bot.send_message(message.chat.id, hello, parse_mode='html')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        # start = types.KeyboardButton(r'/start')
+        play = types.KeyboardButton(r'/play')
+        markup.add(play)
+        bot.send_message(message.chat.id,hello,parse_mode='html', reply_markup=markup)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    hello = f'Привет, <b>{message.from_user.first_name}! Сыграем? </b> \nПравила игры: дойдите до правого нижнего угла.'
-    #bot.send_message(message.chat.id, hello, parse_mode='html')
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    start = types.KeyboardButton(r'/start')
-    play = types.KeyboardButton(r'/play')
-    markup.add(start, play)
-    bot.send_message(message.chat.id,hello,parse_mode='html', reply_markup=markup)
+    @bot.message_handler(commands=['play'])
+    def play_message(message):
+        map_cell = gm(rows, cols)
 
-@bot.message_handler(commands=['play'])
-def play_message(message):
-    map_cell = gm(rows, cols)
+        user_data = {
+            'map': map_cell,
+            'x': 0,
+            'y': 0
+        }
 
-    user_data = {
-        'map': map_cell,
-        'x': 0,
-        'y': 0
-    }
+        maps[message.chat.id] = user_data
 
-    maps[message.chat.id] = user_data
-
-    bot.send_message(message.from_user.id, get_map_str(map_cell, (0, 0)), reply_markup=keyboard)
+        bot.send_message(message.from_user.id, get_map_str(map_cell, (0, 0)), reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_func(query):
-    user_data = maps[query.message.chat.id]
-    new_x, new_y = user_data['x'], user_data['y']
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_func(query):
+        user_data = maps[query.message.chat.id]
+        new_x, new_y = user_data['x'], user_data['y']
 
-    if query.data == 'left':
-        new_x -= 1
-    if query.data == 'right':
-        new_x += 1
-    if query.data == 'up':
-        new_y -= 1
-    if query.data == 'down':
-        new_y += 1
+        if query.data == 'left':
+            new_x -= 1
+        if query.data == 'right':
+            new_x += 1
+        if query.data == 'up':
+            new_y -= 1
+        if query.data == 'down':
+            new_y += 1
 
-    if new_x < 0 or new_x > 2 * cols - 2 or new_y < 0 or new_y > rows * 2 - 2:
-        return None
-    if user_data['map'][new_x + new_y * (cols * 2 - 1)]:
-        return None
+        if new_x < 0 or new_x > 2 * cols - 2 or new_y < 0 or new_y > rows * 2 - 2:
+            return None
+        if user_data['map'][new_x + new_y * (cols * 2 - 1)]:
+            return None
 
-    user_data['x'], user_data['y'] = new_x, new_y
+        user_data['x'], user_data['y'] = new_x, new_y
 
-    if new_x == cols * 2 - 2 and new_y == rows * 2 - 2:
+        if new_x == cols * 2 - 2 and new_y == rows * 2 - 2:
+            bot.edit_message_text(chat_id=query.message.chat.id,
+                                  message_id=query.message.id,
+                                  text="Вы выиграли! :)")
+            return None
+
         bot.edit_message_text(chat_id=query.message.chat.id,
                               message_id=query.message.id,
-                              text="Вы выиграли! :)")
-        return None
-
-    bot.edit_message_text(chat_id=query.message.chat.id,
-                          message_id=query.message.id,
-                          text=get_map_str(user_data['map'], (new_x, new_y)),
-                          reply_markup=keyboard)
+                              text=get_map_str(user_data['map'], (new_x, new_y)),
+                              reply_markup=keyboard)
 
 
-bot.polling(none_stop=False, interval=0)
+    bot.polling(none_stop=False, interval=0)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
-)
+import bot as b
 
-logger = logging.getLogger(__name__)
+if __name__ == '__main__':
+    b.main()
+
